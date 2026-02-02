9803b5b29e82953d6def0959b65fc35b0305a7d3
Shadrack Bett <shadybett540@gmail.com>
2026-02-02 10:19:15 +0300

    created root folders and files

---
+++ backend/run.py
@@ -0,0 +1,4 @@
+# Is the projects entry point necessary for starting project's development server
+# app folder contains all core project logic like routes, models, configurations,
+# migrations folder contains database migration scripts for versioning and handling schema changes
+# tests folder contains automated tests to ensure system reliability
