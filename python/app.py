4c2d94571ebb7ab58fdaed91d48165b0494bff2c
Shadrack Bett <shadybett540@gmail.com>
2026-05-07 10:33:29 +0300

    create python file for exercises

---
+++ python/app.py
@@ -0,0 +1,19 @@
+import math
+
+print('Hello world')
+print('s' * 10)
+s = 2
+course = "    Programming with Mosh"
+length = len(course)
+print(course[0])
+print(course[-1]) 
+print(course[0:3])
+print(length)
+print(course)
+print(course.replace("P", "J"))
+print("swift" in  course)
+print("Pro" not in course)
+print(10 // 3)
+print(round(2.9))
+print(abs(-2.9))    
+print (math.floor(2.1))
